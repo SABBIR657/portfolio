3a91948b73fcbcfadda39596ef2a8cc159ec3cca
unknown <mdsabbirrahman2025@gmail.com>
2025-07-23 01:15:18 +0600

    clearing

--- portfolio-backend/test.js
+++
@@ -1,5 +0,0 @@
-// Run this in a temporary route or test file
-import bcrypt from 'bcryptjs'
-const hash = "$2a$10$N9qo8uLOickgx2ZMRZoMy.MqrqUVs0Z4fqX3WU628iC5YVYvQ9qG6";
-const isMatch = await bcrypt.compare("112233", hash);
-console.log("Password match:", isMatch); // Should be true
